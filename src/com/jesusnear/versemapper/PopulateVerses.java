5005edfc241afcb02ab3ec9f902a121a890a40b6
Hermanovych Danylo <elluyema@gmail.com>
2025-10-19 12:14:02 +0300

    remove hardcode pathes to bible files

--- src/com/jesusnear/versemapper/PopulateVerses.java
+++ src/com/jesusnear/versemapper/PopulateVerses.java
@@ -1,11 +1,14 @@
 package com.jesusnear.versemapper;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import com.jesusnear.versemapper.loaders.VerseFileLoader;
 import com.jesusnear.versemapper.loaders.VersesLoader;
@@ -15,22 +18,22 @@ import com.jesusnear.versemapper.services.VersesService;
 import com.jesusnear.versemapper.utils.JDBCUtilities;
 
 class PopulateVerses {
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private static Map<String, String> loadBibleProperties(String propertiesFilePath) throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(propertiesFilePath);
+        prop.loadFromXML(fis);
 
-    private static final Map<String, String> LANGUAGE_FILE_MAP = Map.of(
-        "en", "D:\\study\\VerseMapper\\data\\bibles\\EN\\web.json",
-        "es", "D:\\study\\VerseMapper\\data\\bibles\\ES\\rv_1909.json",
-        "pt", "D:\\study\\VerseMapper\\data\\bibles\\PT\\blivre.json",
-        "fr", "D:\\study\\VerseMapper\\data\\bibles\\FR\\segond_1910.json",
-        "de", "D:\\study\\VerseMapper\\data\\bibles\\DE\\luther_1912.json",
-        "ru", "D:\\study\\VerseMapper\\data\\bibles\\RU\\synodal.json"
-    );
-
+        return (Map) prop;
+    }
 
     public static void main(String[] args) throws IOException, SQLException {
-        if (args[0] == null) {
+        if (args[0] == null || args[1] == null) {
             System.err.println("Properties file not specified at command line");
             return;
         }
+        
+        Map<String, String> LANGUAGE_FILE_MAP = loadBibleProperties(args[1]);
         
         JDBCUtilities JDBCUtilitiesService = new JDBCUtilities(args[0]);
         
